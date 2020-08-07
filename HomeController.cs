6c68ba4a9f69cf947fec934265b26893e414686d
@Hoangthong97 <Hoangthong97.th@gmail.com>
2020-08-07 14:42:29 +0700

    first commit

---
+++ HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAPI.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			//B1: create project
+			//B2: install package Microsoft.AspNet.WebApi.Cors
+			//B3: Add webconfig cho CROS
+			//B4: Vào webapiconfig trong app_start cấu hình
+			ViewBag.Title = "Home Page";
+
+			return View();
+		}
+	}
+}
